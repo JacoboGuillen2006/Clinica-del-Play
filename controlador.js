e89ebe3d26d4bb1249fc456b94774448fb0e3e9d
Jacobo Guillen 2006 <jacoboguillen79.com>
2022-04-27 17:21:39 -0500

    se guarda el index

---
+++ controlador.js
@@ -0,0 +1,114 @@
+let producto1={
+    nombre: "GTA V PS5",
+    estado: "Nuevo",
+    precio: 150000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/GTA%20PS5.jpg?alt=media&token=fb1b820e-37ee-43c6-8749-c54175c9de62"
+}
+let producto2={
+    nombre: "Spider Man Miles Morales PS5",
+    estado: "Nuevo",
+    precio: 200000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/Spider%20Man%20miles%20morales%20ps5.jfif?alt=media&token=1b0d5d26-0bad-421a-ac5c-390d6b44bd25"
+}
+let producto3={
+    nombre: "Avengers PS5",
+    estado: "Nuevo",
+    precio: 200000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/avengers%20ps5.jpg?alt=media&token=fe3e4856-ae2e-406b-8bb6-774a8e340e68"
+}
+let producto4={
+    nombre: "Cable Lan",
+    estado: "Nuevo",
+    precio: 200000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/cable%20lan.jfif?alt=media&token=4e663314-4e06-4e9a-ba24-31960eae07b6"
+}
+let producto5={
+    nombre: "Control PS4",
+    estado: "Nuevo",
+    precio: 82000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/control%20de%20play%204.jfif?alt=media&token=edba52be-50d7-4bc7-acce-5cf1493c9c46"
+}
+let producto6={
+    nombre: "Control PS4 Dorado",
+    estado: "Nuevo",
+    precio: 120000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/control%20play%204%20dorado.jfif?alt=media&token=61d14a55-1bd3-4e1f-86c2-ae35c90a4dc3"
+}
+let producto7={
+    nombre: "Control PS5",
+    estado: "Nuevo",
+    precio: 315000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/control%20play%205.jfif?alt=media&token=a2e21d89-225b-48b6-bdf3-e33d63a71060"
+}
+let producto8={
+    nombre: "Fifa 22 PS5",
+    estado: "Nuevo",
+    precio: 249000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/fifa%2022%20play%205.jpg?alt=media&token=dc009a86-d2b0-4955-91e3-bd34b6bd5f44"
+}
+let producto9={
+    nombre: "Fifa 22 PS4",
+    estado: "Nuevo",
+    precio: 200000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/fifa%2022%20ps4.jpg?alt=media&token=d43cef89-e3f0-4279-aa76-a0124626338c"
+}
+let producto10={
+    nombre: "GTA V PS4",
+    estado: "Nuevo",
+    precio: 100000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/gta%20ps4.jpg?alt=media&token=7e17963a-ebcb-4524-8244-c725b4858755"
+}
+let producto11={
+    nombre: "Play Station 2",
+    estado: "Segunda",
+    precio: 90000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/play%202.jpg?alt=media&token=7770a578-7c7a-49fd-83e9-b8b3ac2add39"
+}
+let producto12={
+    nombre: "Play Station 4",
+    estado: "Segunda",
+    precio: 150000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/play%204.jpg?alt=media&token=bcbf9f19-94d5-4499-b245-8862c5f6521b"
+}
+let producto13={
+    nombre: "Play Station 4 Pro",
+    estado: "Segunda",
+    precio: 195000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/play%204%20pro.jpg?alt=media&token=47e7ad4c-4108-4703-a9be-93afd55fedc0"
+}
+let producto14={
+    nombre: "Play Station 5",
+    estado: "Primero",
+    precio: 350000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/play%205.jpg?alt=media&token=34821ecd-a1cd-457e-bbc3-0a8290b75055"
+}
+let producto15={
+    nombre: "assassin's creed valhalla ps5",
+    estado: "Primero",
+    precio: 250000,
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-d7aa0.appspot.com/o/assassin's%20creed%20ps5.jpg?alt=media&token=1d6ff26c-53be-4426-9f05-6d9b1191b9db"
+}
+let productos = Array (
+producto1,
+producto2,
+producto3,
+producto4,
+producto5,
+producto6,
+producto7,
+producto8,
+producto9,
+producto10,
+producto11,
+producto12,
+producto13,
+producto14,
+producto15,
+)
+console.log(productos)
+
+productos.forEach(function(producto){
+console.log(producto)
+console.log(producto.nombre)
+})
+
